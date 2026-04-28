a6f97ea5ee08d68313ed0d2f2bbed5a39d215cca
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2026-04-28 22:53:14 +0200

    20260429 upd

--- 20260429/exam.py
+++ 20260429/exam.py
@@ -80,12 +80,14 @@ def magic(h_rec):
 
 class TestUnfolding(unittest.TestCase):
   def test_pvalue(self):
-    # this file should be used fore responce matrix
+    # this file should be used for responce matrix
     fname1 = 'ntuples-mc/TTJets_TuneZ2_7TeV-madgraph-tauola/00000/ttbarSel_merged.root'
     # this file should be used fore actual comparison (like data)
     fname2 = 'ntuples-mc/TTJets_TuneZ2_7TeV-madgraph-tauola/00001/ttbarSel_merged.root'
+    # download these ROOT files from here (~ 1 GB):
+    # https://cernbox.cern.ch/s/UmbXF1XxVrT4whQ
 
-    # do something with fname1 here...
+    # do something with fname1 here (responce matrix...)
 
     # now process fname2
     t2 = ROOT.TChain('tree')
